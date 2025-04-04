c3d0161d90debf30eab92ec610daba18e42f0d07
Nguyen Manh <manhhn01@gmail.com>
2025-04-04 09:38:11 +0700

    feat: update headline

--- src/frontend/src/pages/Explore/index.tsx
+++ src/frontend/src/pages/Explore/index.tsx
@@ -7,17 +7,16 @@ const ExplorePage = () => {
         <div className="mx-auto flex size-full max-w-[990px] flex-col-reverse items-start justify-center gap-[54px] p-12 md:flex-row md:items-end md:py-0 lg:items-center">
           <div className="flex flex-1 flex-col justify-center md:py-12 lg:py-0">
             <h2 className="mb-2 text-h4 font-bold leading-h4">
-              Mua 1 được 2 với CharMarket, ICP NFT market
+              NFT Ownership equals AI Earnings
             </h2>
             <h3 className="mb-4 text-base text-opacityColor-70">
-              Mua NFT trên CharMarket bạn đồng thời sở hữu quyền trò chuyện với
-              Character đó trên CharClub.AI
+              Earn passive income when users pay to chat with your AI character.
             </h3>
             <button
               type="button"
               className="w-fit rounded-xl bg-white px-4 py-3 text-base font-semibold text-primary"
             >
-              Tìm hiểu thêm
+              Learn more
             </button>
           </div>
 
